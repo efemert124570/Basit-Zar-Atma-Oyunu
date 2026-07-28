f79dd822f89bc58cfcd0072a3357085d6488f22c
Efe Mert <efemert192@gmail.com>
2026-07-28 16:14:00 +0300

    1 oyuncu secenegi eklendi

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 int main(){
-int oyuncusayisi,zardeger;
+int oyuncusayisi,zardeger1;
 printf("Zar Atma Oyununa Hosgeldiniz\n");
 printf("Bu Oyun 1-6 Arasındaki Kisilerle Oynanabilir\n");
 printf("Lutfen Kisi Sayisini Giriniz: ");
 scanf("%d",&oyuncusayisi);
+
+switch (oyuncusayisi)
+{
+case 1:
+printf("Zar Cevriliyor\n");
+sleep(1);
+ srand(time(NULL));
+zardeger1=rand()%6;
+printf("Oyuncu 1 Gelen Zar = %d",zardeger1);
+}
 
 
 
