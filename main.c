82889de3922bc677baf60f9551c3dc3c032c0f98
Efe Mert <efemert192@gmail.com>
2026-07-28 16:02:13 +0300

    Oyuncu Sayisini alma kismi eklendi

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 
 int main(){
 int oyuncusayisi,zardeger;
-
+printf("Zar Atma Oyununa Hosgeldiniz\n");
+printf("Bu Oyun 1-6 Arasındaki Kisilerle Oynanabilir\n");
+printf("Lutfen Kisi Sayisini Giriniz: ");
+scanf("%d",&oyuncusayisi);
 
 
 
