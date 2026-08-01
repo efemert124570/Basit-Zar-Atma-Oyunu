5f10dde967e85d5c0bbb722c4137680c0b187f2b
Efe Mert <efemert192@gmail.com>
2026-08-01 16:20:12 +0300

    Fazla rand ve srandlar silindi

--- main.c
+++ main.c
@@ -8,143 +8,109 @@ int main() {
 	printf("Bu Oyun 1-6 Arasındaki Kisilerle Oynanabilir\n");
 	printf("Lutfen Kisi Sayisini Giriniz: ");
 	scanf("%d",&oyuncusayisi);
-
+srand(time(NULL));
+zardeger1=rand()%6+1;
+zardeger2=rand()%6+1;
+zardeger3=rand()%6+1;
+zardeger4=rand()%6+1;
+zardeger5=rand()%6+1;
+zardeger6=rand()%6+1;
 	switch (oyuncusayisi)
 	{
 	case 1:
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger1=rand()%6+1;
 		printf("Oyuncu 1 Gelen Zar = %d",zardeger1);
 		break;
 
 	case 2:
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger1=rand()%6+1;
+		
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger2=rand()%6+1;
+		
 		printf("Oyuncu 2 Gelen Zar = %d",zardeger2);
 		break;
 
 	case 3:
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger1=rand()%6+1;
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger2=rand()%6+1;
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger3=rand()%6+1;
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
 		break;
 	case 4:
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger1=rand()%6+1;
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger2=rand()%6+1;
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger3=rand()%6+1;
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger4=rand()%6+1;
 		printf("Oyuncu 4 Gelen Zar = %d\n",zardeger4);
 
 		break;
 	case 5:
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger1=rand()%6+1;
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger2=rand()%6+1;
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger3=rand()%6+1;
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger4=rand()%6+1;
 		printf("Oyuncu 4 Gelen Zar = %d\n",zardeger4);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger5=rand()%6+1;
 		printf("Oyuncu 5 Gelen Zar = %d\n",zardeger5);
 		break;
 
 	case 6:
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger1=rand()%6+1;
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger2=rand()%6+1;
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger3=rand()%6+1;
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger4=rand()%6+1;
 		printf("Oyuncu 4 Gelen Zar = %d\n",zardeger4);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger5=rand()%6+1;
 		printf("Oyuncu 5 Gelen Zar = %d\n",zardeger5);
 
 		printf("Zar Cevriliyor\n");
 		sleep(1);
-		srand(time(NULL));
-		zardeger6=rand()%6+1;
 		printf("Oyuncu 6 Gelen Zar = %d\n",zardeger6);
 		break;
 
