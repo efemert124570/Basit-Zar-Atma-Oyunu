04a2c777fcbac8537d1e7b19a3fe88946071016b
Efe Mert <efemert192@gmail.com>
2026-08-02 18:55:56 +0300

    Basit açıklama eklendi

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main() {
+//Değişkenlerimiz
 	int oyuncusayisi,zardeger1,zardeger2,zardeger3,zardeger4,zardeger5,zardeger6;
+	//Bilgilendirmeyi yazzdıran kısım
 	printf("Zar Atma Oyununa Hosgeldiniz\n");
 	printf("Bu Oyun 1-6 Arasındaki Kisilerle Oynanabilir\n");
 	printf("Lutfen Kisi Sayisini Giriniz: ");
+//Kişi sayısını alacsak yer
 	scanf("%d",&oyuncusayisi);
-srand(time(NULL));
+//Zar değerlerinin belirlendiği yer
+	srand(time(NULL));
 zardeger1=rand()%6+1;
 sleep(0.1);
 zardeger2=rand()%6+1;
@@ -20,20 +24,23 @@ sleep(0.1);
 zardeger5=rand()%6+1;
 sleep(0.1);
 zardeger6=rand()%6+1;
-	switch (oyuncusayisi)
+//Verdiğimiz oyuuncu sayısına göre seçim yapan yer	
+switch (oyuncusayisi)
 	{
 	case 1:
-		printf("Zar Cevriliyor\n");
+	//Oyuncu 1	
+	printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 1 Gelen Zar = %d",zardeger1);
 		break;
 
 	case 2:
-		printf("Zar Cevriliyor\n");
+	//Oyuncu 1
+	printf("Zar Cevriliyor\n");
 		sleep(1);
 		
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
-
+//Oyuncu 2
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		
@@ -41,85 +48,90 @@ zardeger6=rand()%6+1;
 		break;
 
 	case 3:
-		printf("Zar Cevriliyor\n");
+	//Oyuncu 1
+	printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
-
+//Oyuncu 2
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
-
+//Oyuncu 3
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
 		break;
 	case 4:
+	//Oyuncu 1
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
-
+//Oyuncu 2
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
-
+//Oyuncu 3
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
-
+//Oyuncu 4
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 4 Gelen Zar = %d\n",zardeger4);
 
 		break;
 	case 5:
+	//Oyuncu 1
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
-
+//Oyuncu 2
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
-
+//Oyuncu 3
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
-
+//Oyuncu 4
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 4 Gelen Zar = %d\n",zardeger4);
-
+//Oyuncu 5
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 5 Gelen Zar = %d\n",zardeger5);
 		break;
 
 	case 6:
+	//Oyuncu 1
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 1 Gelen Zar = %d\n",zardeger1);
-
+//Oyuncu 2
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 2 Gelen Zar = %d\n",zardeger2);
-
+//Oyuncu 3
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 3 Gelen Zar = %d\n",zardeger3);
-
+//Oyuncu 4
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 4 Gelen Zar = %d\n",zardeger4);
-
+//Oyuncu 5
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 5 Gelen Zar = %d\n",zardeger5);
-
+//Oyuncu 6
 		printf("Zar Cevriliyor\n");
 		sleep(1);
 		printf("Oyuncu 6 Gelen Zar = %d\n",zardeger6);
 		break;
 
 	default:
+	//Hatalı girişte yazdırılacak kısım
 		printf("Yanlis veya Hatali Sayi Girisi Yaptiniz\n");
 		printf("Programdan cikiliyor");
 	}
